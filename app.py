e146d79a5d7e8b96c50b5967e06a717341509a04
Marcos Batista <104324784+Brutomemo@users.noreply.github.com>
2026-05-05 11:49:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2006,7 +2006,7 @@ else:
             st.markdown("<p style='color: #bbb;'>Este módulo traduz a matriz matemática gerada acima em um relatório estratégico que visa o aperfeiçoamento técnico contínuo do Negociador.</p>", unsafe_allow_html=True)
 
             if st.button("🤖 GERAR RELATÓRIO ESTATÍSTICO DESCRITIVO"):
-                
+                st.markdown("<div style='margin-bottom: 25px;'></div>", unsafe_allow_html=True)
                 with st.spinner("Estruturando matrizes e consultando Cientista de Dados IA..."):
                     try:
                         import ia_estatistica 
@@ -2059,8 +2059,7 @@ else:
                                 st.json(payload_ia)
                                 st.markdown("**JSON Retornado pela IA:**")
                                 st.json(relatorio_json)
-
-                            st.markdown("<div style='margin-bottom: 30px;'></div>", unsafe_allow_html=True)
+                            
                             st.markdown("---")
                             st.markdown("### 🖨️ Exportar Relatório")
                             
